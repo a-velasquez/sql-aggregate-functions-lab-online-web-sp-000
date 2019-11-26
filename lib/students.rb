7abf00b390a12b65354e2da1f9ab73b22468c103
Alex Velasquez <jtinnesco@gmail.com>
2019-11-26 23:53:52 +0000

    Automatically backed up by Learn

--- lib/students.rb
+++ lib/students.rb
@@ -5,11 +5,11 @@ def highest_student_gpa
 end
 
 def lowest_student_gpa
-  'SELECT MIN(gpa) FROM students;'
+  'SELECT MIN(students.gpa) FROM students;'
 end
 
 def average_student_gpa
-  'SELECT AVG(gpa) FROM students;'
+  'SELECT AVG(students.gpa) FROM students;'
 end
 
 def total_tardies_for_all_students
